bff0f394cdcc753b41b0c3e70d260ebb823c131d
your name <your email.com>
2022-01-12 14:41:37 +0800

    xxx

---
+++ src/TailWind_assets/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "tailwindcss/tailwind.css";
+
+function App() {
+  return <div className="bg-slate-500">hello</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.{react,js,jsx}"],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
